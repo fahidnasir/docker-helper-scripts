088273dbd57a3e8f9a105a162d1fc73536d0398c
Fahid Nasir <fahidnasir@hotmail.com>
2019-05-15 12:22:36 +0500

    Added: Docker volume clone and Get info script.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Docker Helper Scripts
+
+This repository contains shell scripts for docker to automate/help routine operations.
+
+## docker_volume_clone.sh
+
+This script will create a duplicate of an existing data volume.
+
+Example: If you want the volume from production environment for development version to recreate the same experience at development.
+
+## docker_volume_get_info.sh
+
+This script will list all the volumes with the attached stopped or running container IDs and disk space size.
+
+Example: If you want the volume from production environment for development version to recreate the same experience at development.
---
+++ docker_volume_clone.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#Author: Fahid Nasir
+
+#Helper script that can help copying the data from one docker volume to a new
+
+#data volume. The script is mainly useful if you are using named volumes
+
+# Check the volume names provided and prompt if not available
+SOURCE_VOLUME=$1
+DESTINATION_VOLUME=$2
+
+if [ "$SOURCE_VOLUME" = "" ]
+then
+	echo "Please provide a source volume name"
+	read SOURCE_VOLUME
+fi
+
+if [ "$DESTINATION_VOLUME" = "" ] 
+then
+	echo "Please provide a destination volume name"
+	read DESTINATION_VOLUME
+fi
+
+#Check if the source volume name does exist
+docker volume inspect $SOURCE_VOLUME > /dev/null 2>&1
+if [ "$?" != "0" ]
+then
+	echo "The source volume \"$SOURCE_VOLUME\" does not exist"
+	read -p "Press any key to continue... " -n 1 -s
+	exit
+fi
+
+#Now check if the destinatin volume name does not yet exist
+docker volume inspect $DESTINATION_VOLUME > /dev/null 2>&1
+
+if [ "$?" = "0" ]
+then
+	echo "The destination volume \"$DESTINATION_VOLUME\" already exists"
+	read -p "Press any key to continue... " -n 1 -s
+	exit
+fi
+
+
+
+echo "Creating destination volume \"$DESTINATION_VOLUME\"..."
+docker volume create --name $DESTINATION_VOLUME
+echo "Copying data from source volume \"$SOURCE_VOLUME\" to destination volume \"$DESTINATION_VOLUME\"..."
+docker run --rm -it -v $SOURCE_VOLUME:/from -v $DESTINATION_VOLUME:/to busybox sh -c "cd /from ; cp -av . /to"
